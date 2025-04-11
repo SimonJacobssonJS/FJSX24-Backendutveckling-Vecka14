9602ea50af4b4c715ac40358d1002d79fe91308b
Simon Jacobsson <simon.jacobsson@chasacademy.se>
2025-04-11 08:08:27 +0200

    Update app.js

--- app.js
+++ app.js
@@ -6,3 +6,7 @@ const PORT = 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
+
+app.get('/api/greet', (req, res) => {
+  res.send('Hello World!');
+});
